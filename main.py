325b61c8f16e6a120da3af5425a3e2095363e8c9
анна <lekomtseva394@inbox.ru>
2022-10-11 18:38:08 +0500

    commit3_2

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ for i in range(N):
     a.append(int(input("число: ")))
 print(a)
 
-for i in range(N - 1):
-    for j in range(N - i - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
+i = int(0)
+i = int(input("1 - по возрастанию; 2 - по убыванию: "))
+
+if i == 1:
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] > a[j + 1]:
+             a[j], a[j + 1] = a[j + 1], a[j]
+
+if i == 2:
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] < a[j + 1]:
+              a[j], a[j + 1] = a[j + 1], a[j]
+
 print(a)
